2d85c63f26fe0b606e9f9b4e1f1bfb9dd80a4019
Stephan Smirnoff <foxelyss@gmail.com>
2025-12-15 21:04:57 +0700

    Patching min length

--- src/DTO/PointPatch.cs
+++ src/DTO/PointPatch.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 namespace TravelBooking.DTO;
 
 public record PointPatch(
-    [MinLength(2), MaxLength(128)] string? name,
-    [MinLength(2), MaxLength(128)] string? region,
-    [MinLength(2), MaxLength(128)] string? city)
+    [MinLength(1), MaxLength(128)] string? name,
+    [MinLength(1), MaxLength(128)] string? region,
+    [MinLength(1), MaxLength(128)] string? city)
 { };
--- src/DTO/PointRegistration.cs
+++ src/DTO/PointRegistration.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 namespace TravelBooking.DTO;
 
 public record PointRegistration(
-    [Required, MinLength(2), MaxLength(128)] string name,
-    [Required, MinLength(2), MaxLength(128)] string region,
-    [Required, MinLength(2), MaxLength(128)] string city)
+    [Required, MinLength(1), MaxLength(128)] string name,
+    [Required, MinLength(1), MaxLength(128)] string region,
+    [Required, MinLength(1), MaxLength(128)] string city)
 { };
